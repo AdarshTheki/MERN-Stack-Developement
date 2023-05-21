ab85a062bbcafe79196944d12a9126454847dca2
Adarsh Verma <adarshverma549@gmail.com>
2023-05-21 19:28:37 +0530

    modify index file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import ReactDOM from "react-dom/client";
 // import App from "./Router/App";
 // import Hooks from "./Projects/Hooks/Hooks";
 // import App from "./Projects/Restaurant/App";
-import App from "./Projects/TodoList/App";
+// import App from "./Projects/TodoList/App";
+// import App from "./Projects/WeatherApp/App";
+import App from "./Projects/PortFolio/App";
 
 import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById('root'));
