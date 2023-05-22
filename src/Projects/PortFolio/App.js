63636fc7a40e722f20b5a0cd6a6f8cd9cfbae276
Adarsh Verma <adarshverma549@gmail.com>
2023-05-22 17:23:33 +0530

    All Modified File Update

--- src/Projects/PortFolio/App.js
+++ src/Projects/PortFolio/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import Website from './Website';
-import Netflix from './Netflix';
-import { BrowserRouter as Route, Link, Routes, Router  } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Netflix from './Components/Netflix';
+import Project from './Pages/Project';
+import About from './Pages/About.js';
+import Contact from './Pages/Contact.js';
+import Service from './Pages/Service.js';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <Website/>
-      
+    <Router>
+        <Header/>
+      <Routes>
+        <Route exact path='/' Component={Home}/>
+        <Route path='/about' Component={About}/>
+        <Route path='/contact' Component={Contact}/>
+        <Route path='/project' Component={Project}>
+          <Route path='netflix' Component={Netflix}/>
+        </Route>
+        <Route path='/service' Component={Service}/>
+        <Route path='/*' element={<><h1>404 Error</h1></>}/>
+      </Routes>
+    </Router>
       
     </>
   )
--- src/Projects/PortFolio/usabilityhub/script.js
+++
@@ -1,5 +0,0 @@
-const menu = document.getElementById('menu');
-menu.addEventListener('click', () => {
-    const navLink = document.querySelector('.nav-links')
-    navLink.classList.toggle('sidebar');
-})
