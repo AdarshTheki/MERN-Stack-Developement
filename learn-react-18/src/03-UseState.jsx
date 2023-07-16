4b5c699f9a30e97ff3faedd4e8176a1b802abee4
Adarsh Verma <adarshverma549@gmail.com>
2023-07-16 18:05:18 +0530

    useState

--- learn-react-18/src/03-UseState.jsx
+++ learn-react-18/src/03-UseState.jsx
@@ -1,69 +1,81 @@
 import "./App.css";
 import { useState } from "react";
-import {data} from './Data.js'
+import { data } from "./Data.js";
 
-function UseState() {
-  
+function ModifyValue() {
   const [value, setValue] = useState(0);
   const handleChange = () => {
     setTimeout(() => {
       console.log("Clicked");
       setValue((curr) => {
-        return curr + 1
+        return curr + 1;
       });
-    },2000);
+    }, 2000);
   };
 
-  //// Second Example:
-  // const [person, setPerson] = useState({
-  //   name: "Adarsh",
-  //   age: 24,
-  //   hobby: "Summing a Pull of the Earth",
-  // });
-  // const displayPerson = () => {
-  //   setPerson({ ...person, name: "Ayush Verma"});
-  // };
-
-  //// First Example:
-  // const [people, setPeople] = useState(data);
-  // const clearAllItems = () => {
-  //   setPeople([]);
-  // };
-  // const removeItems = (id) => {
-  //   setPeople(people.filter((person) => person.id !== id));
-  // };
   return (
     <>
       <h1>{value}</h1>
-      <button type='button' onClick={handleChange}>
-        Increase
-      </button>
-
-      {/* Second Example: */}
-      {/* <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>Enjoy: {person.hobby}</h3>
-      <button type='button' onClick={displayPerson}>
-        Show John
-      </button> */}
-
-      {/* First Example: */}
-      {/* {people.map((ele) => {
-        const { id, name } = ele;
-        return (
-          <div key={id}>
-            <h3>{name}</h3>
-            <button type='button' onClick={() => removeItems(id)}>
-              Remove One
-            </button>
-          </div>
-        );
-      })}
-      <button type='button' onClick={clearAllItems}>
-        Remove All
-      </button> */}
+      <button onClick={handleChange}>Increase</button>
     </>
   );
 }
 
-export default UseState;
+function ModifyDynamicData() {
+  const [person, setPerson] = useState({
+    name: "Adarsh",
+    age: 24,
+    hobby: "Summing a Pull of the Earth",
+  });
+  const displayPerson = () => {
+    setPerson({ ...person, name: "Ayush Verma" });
+  };
+
+  return (
+    <>
+      <h3>
+        Name: {person.name} & Age: {person.age}
+      </h3>
+      <h3>Enjoy: {person.hobby}</h3>
+      <button onClick={displayPerson}>Show John</button>
+    </>
+  );
+}
+
+const TodoList = () => {
+  const [people, setPeople] = useState(data);
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+  const removeItems = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
+  return (
+    <>
+      {people.map((ele) => {
+        const { id, name } = ele;
+        return (
+          <div key={id}>
+            <h3>{name}</h3>
+            <button onClick={() => removeItems(id)}>Remove One</button>
+          </div>
+        );
+      })}
+      <button onClick={clearAllItems}>Remove All</button>
+    </>
+  );
+};
+
+const Main = () => {
+  return (
+    <div>
+      <TodoList />
+      <hr />
+      <ModifyDynamicData />
+      <hr />
+      <ModifyValue />
+    </div>
+  );
+};
+
+export default Main;
