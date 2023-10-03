85c5f799b84863278df044ccd49c9cfc2495b1be
Adarsh Verma <adarshverma549@gmail.com>
2023-10-03 18:32:03 +0530

    modify the authenticate the current user with edit post

--- authenticate-with-appwrite/src/App.jsx
+++ authenticate-with-appwrite/src/App.jsx
@@ -1,10 +1,10 @@
-import { useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { authServices } from "./AppWrite/Auth";
-import { login, logout } from "./Redux/authSlice";
-import { Outlet } from "react-router-dom";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { authServices } from './AppWrite/Auth';
+import { login, logout } from './Redux/authSlice';
+import { Outlet } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -29,17 +29,24 @@ const App = () => {
   return (
     <div>
       {!loading ? (
-        <div className='min-h-screen border flex flex-wrap content-between bg-gray-300'>
+        <div className='min-h-screen flex flex-wrap content-between'>
           <div className='w-full block'>
             <Header />
-            <main>
-              TODO: <Outlet />
+            <main className='bg-neutral min-h-[50vh]'>
+              <h2 className='text-center text-primary text-xl'>
+                This is a Simple Todo App built with React & Appwrite DataBase.
+              </h2>
+              <p className='text-center text-sm text-error'>
+                Note: Only logged in users can see this page. Please login to see the posts and You can edit post. Thank
+                you. :)
+              </p>
+              <Outlet />
             </main>
             <Footer />
           </div>
         </div>
       ) : (
-        <h1 className="text-center text-4xl font-bold mt-24">Loading...</h1>
+        <h1 className='text-center text-4xl font-bold mt-24'>Loading...</h1>
       )}
     </div>
   );
