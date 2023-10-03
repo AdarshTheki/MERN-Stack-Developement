7312f9c07389fab1ffa7ef1c1cdfa62a1ffa1622
Adarsh Verma <adarshverma549@gmail.com>
2023-10-03 18:23:04 +0530

    Add tailwind config file with daisyui plugin

--- authenticate-with-appwrite/tailwind.config.js
+++ authenticate-with-appwrite/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark', 'night'],
+  },
 };
 
+// adding in index.html
+// <html data-theme="dark"></html>
