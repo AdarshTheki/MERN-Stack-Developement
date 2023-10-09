7280a241083bea0bf7b1629a6fc77455d5e3342f
Adarsh Verma <adarsh878856@gmail.com>
2023-10-09 16:44:53 +0530

    modify the main components fille

--- frontend_projetcts/todo_app_with_redux_toolkit/src/main.jsx
+++ frontend_projetcts/todo_app_with_redux_toolkit/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
-import './main.css'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
