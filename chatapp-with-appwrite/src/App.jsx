beb79252895d1b762ef19131b9b430265ea0d227
Adarsh Verma <adarshverma549@gmail.com>
2023-10-27 08:59:18 +0530

    adding registerPage components in route

--- chatapp-with-appwrite/src/App.jsx
+++ chatapp-with-appwrite/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from './pages/LoginPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PrivateRoutes from './components/PrivateRoutes';
 import { AuthProvider } from './utiles/AuthContext';
+import RegisterPage from './pages/RegisterPage';
 
 export default function App() {
    return (
@@ -14,6 +15,7 @@ export default function App() {
                   <Route path='/' element={<Room />} />
                </Route>
                <Route path='/login' element={<LoginPage />} />
+               <Route path='/register' element={<RegisterPage />} />
             </Routes>
          </AuthProvider>
       </BrowserRouter>
