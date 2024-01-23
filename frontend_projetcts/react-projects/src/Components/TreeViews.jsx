7a35e127b93c33441338f129117c01d3963e4e20
Adarsh Verma <adarshverma549@gmail.com>
2024-01-23 21:40:13 +0530

    adding a Tree View Components / UI / Navigation Menu

---
+++ frontend_projetcts/react-projects/src/Components/TreeViews.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { menus } from './tree-views'; // importing a data tree view structure
+
+// Tree view components / menu UI components / recursive navigation menu
+export default function TreeViews() {
+    return (
+        <div className='wrapper'>
+            <MenuList list={menus} />
+        </div>
+    );
+}
+
+function MenuList({ list = [] }) {
+    return (
+        <ul className='menu-list'>
+            {list && list.length
+                ? list.map((listItem) => <MenuItem key={listItem.label} item={listItem} />)
+                : null}
+        </ul>
+    );
+}
+
+function MenuItem({ item }) {
+    const [currentChild, setCurrentChild] = useState({});
+
+    function toggleChild(child) {
+        setCurrentChild({ ...currentChild, [child]: !currentChild[child] });
+    }
+    console.log(currentChild);
+
+    return (
+        <li className='menu-item'>
+            <div>
+                <p>{item.label}</p>
+                {item && item.children ? (
+                    <span onClick={() => toggleChild(item.label)}>
+                        {currentChild[item.label] ? '-' : '+'}
+                    </span>
+                ) : null}
+            </div>
+            {item && item.children && currentChild[item.label] ? (
+                <MenuList list={item.children} />
+            ) : null}
+        </li>
+    );
+}
