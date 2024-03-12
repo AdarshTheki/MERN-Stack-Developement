0f927cb9b87cafd30075fd999c3f6f7665e51602
adarshtheki <adarshverma549@gmail.com>
2024-03-12 12:04:14 +0530

    React Design Concepts

--- react-design-and-concepts/react-projects/src/App.jsx
+++ react-design-and-concepts/react-projects/src/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
-// import Tooltip from './Components/Tooltip';
+// import Pagination from './Components/1_Pagination';
+// import DigitalClock from './Components/2_DigitalClock';
+// import CountdownTimer from './Components/3_CountdownTimer';
+// import StepProgressbar from './Components/4_StepProgressbar';
+// import Tooltip from './Components/5_Tooltip';
+// import CurrencyConverter from './Components/6_CurrencyConverter';
+// import FilterCards from './Components/7_FilterCards';
+// import TipCalculator from './Components/8_TipCalculator';
+// import MusicPlayer from './Components/9_MusicPlayer';
+// import ProgressBar from './Components/10_ProgressBar';
+// import BmiCalculator from './Components/11_BmiCalculator';
+// import ButtonRippleEffect from './Components/12_ButtonRippleEffect';
+// import DragAndDrop from './Components/13_DragAndDropSides';
+import FormValidation from './Components/14_FormValidation';
 // import DatePicker from './Components/DatePicker';
 // import AnimationSpinner from './Components/AnimationSpinner';
 // import DragAndDrop from './Components/DragAndDrop';
@@ -28,12 +41,14 @@ import React from 'react';
 // import CharacterCount from './Components/CharacterCount';
 // import ProgressiveImage from './Components/ProgressiveImage';
 // import Loader from './Components/Loader';
-import TodoList from './TodoList/TodoList';
+
+// Practices Question Todo Lists
+// import TodoList from './TodoList/TodoList';
 
 export default function App() {
     return (
         <div>
-            <TodoList />
+            <FormValidation />
         </div>
     );
 }
