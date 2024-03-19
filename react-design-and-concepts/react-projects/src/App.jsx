d60747c20cbbc04057c445cb52ff6e8fa7596a56
adarshtheki <adarshverma549@gmail.com>
2024-03-19 21:39:09 +0530

    packages Install

--- react-design-and-concepts/react-projects/src/App.jsx
+++ react-design-and-concepts/react-projects/src/App.jsx
@@ -1,54 +1,10 @@
 import React from 'react';
-// import Pagination from './Components/1_Pagination';
-// import DigitalClock from './Components/2_DigitalClock';
-// import CountdownTimer from './Components/3_CountdownTimer';
-// import StepProgressbar from './Components/4_StepProgressbar';
-// import Tooltip from './Components/5_Tooltip';
-// import CurrencyConverter from './Components/6_CurrencyConverter';
-// import FilterCards from './Components/7_FilterCards';
-// import TipCalculator from './Components/8_TipCalculator';
-// import MusicPlayer from './Components/9_MusicPlayer';
-// import ProgressBar from './Components/10_ProgressBar';
-// import BmiCalculator from './Components/11_BmiCalculator';
-// import ButtonRippleEffect from './Components/12_ButtonRippleEffect';
-// import DragAndDrop from './Components/13_DragAndDropSides';
-import FormValidation from './Components/14_FormValidation';
-// import DatePicker from './Components/DatePicker';
-// import AnimationSpinner from './Components/AnimationSpinner';
-// import DragAndDrop from './Components/DragAndDrop';
-// import Accordion from './Components/Accordion';
-// import RandomColor from './Components/RandomColor';
-// import StarRating from './Components/StarRating';
-// import ImageSlider from './Components/ImageSlider';
-// import LoadMore from './Components/LoadMore';
-// import TreeViews from './Components/TreeViews';
-// import QRCodeGenerator from './Components/QRCodeGenerator';
-// import ThemeSwitch from './Components/ThemeSwitch';
-// import ScrolledIndicator from './Components/ScrolledIndicator';
-// import TabbedContent from './Components/TabbedContent';
-// import ModalDialog from './Components/ModalDialog';
-// import GithubProfileFinder from './Components/GithubProfileFinder';
-// import SearchAutoComplete from './Components/SearchAutoComplete';
-// import TicTacToe from './Components/TicTacToe';
-// import UseFetchComponent from './Components/UseFetchComponent';
-// import OnClickOutside from './Components/OnClickOutside';
-// import WindowResize from './Components/WindowResize';
-// import ScrollToTopBottom from './Components/ScrollToTopBottom';
-// import ScrollToSection from './Components/ScrollToSection';
-// import FeatureFlag from './Components/FeatureFlag';
-// import LocalStorage from './Components/LocalStorage';
-// import ErrorBoundary from './Components/ErrorBoundary';
-// import CharacterCount from './Components/CharacterCount';
-// import ProgressiveImage from './Components/ProgressiveImage';
-// import Loader from './Components/Loader';
-
-// Practices Question Todo Lists
-// import TodoList from './TodoList/TodoList';
+import Pagination from './Components/4_StepProgressbar';
 
 export default function App() {
     return (
         <div>
-            <FormValidation />
+            <Pagination />
         </div>
     );
 }
